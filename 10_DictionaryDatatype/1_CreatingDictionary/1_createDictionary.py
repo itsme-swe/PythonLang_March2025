b8dd4daab95fc29ca34fae7f3dab2cd302c63f4a
itsme-swe <me.harshmehra07@gmail.com>
2025-03-07 21:08:01 +0530

    update

---
+++ 10_DictionaryDatatype/1_CreatingDictionary/1_createDictionary.py
@@ -0,0 +1,10 @@
+d1 = {1: "Harsh", 2: "Mehra", 3: 69}
+
+print(d1)  # {1: 'Harsh', 2: 'Mehra', 3: 69}
+
+print()
+
+# Using constructor function
+d2 = dict(a="Volvo", b="Honda", c="BMW")
+
+print(d2)  # {'a': 'Volvo', 'b': 'Honda', 'c': 'BMW'}
