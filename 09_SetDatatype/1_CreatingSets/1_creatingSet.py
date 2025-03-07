7e7d2316c94818fd44aa71c5c41d13d5ef496434
itsme-swe <me.harshmehra07@gmail.com>
2025-03-07 09:43:29 +0530

    update

---
+++ 09_SetDatatype/1_CreatingSets/1_creatingSet.py
@@ -0,0 +1,5 @@
+s1 = {"harsh", 32, 69.5, True}
+
+print(s1)  # {32, True, 69.5, 'harsh'}
+
+print(type(s1)) # <class 'set'>
---
+++ 09_SetDatatype/1_CreatingSets/2_creatingSetUsingFunction.py
@@ -0,0 +1,5 @@
+s1 = set({20, 30, 15, 20, "Harsh", True})
+
+print(s1)  # {True, 20, 'Harsh', 30, 15}
+
+print(len(s1))
