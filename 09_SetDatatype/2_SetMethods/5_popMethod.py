7eea283b8b2f52e9b80fee1c92e9edabd522fd76
itsme-swe <me.harshmehra07@gmail.com>
2025-03-07 18:01:22 +0530

    update

---
+++ 09_SetDatatype/2_SetMethods/5_popMethod.py
@@ -0,0 +1,3 @@
+s1 = {2, 5, 7, 1, 10}
+
+print(s1.pop()) # pop() method removes random element from set -- 1
---
+++ 09_SetDatatype/2_SetMethods/8_differenceMethod.py
@@ -0,0 +1,7 @@
+A = {10, 20, 30, 40, 80}
+
+B = {100, 30, 80, 40, 60}
+
+print(A.difference(B))  # {10, 20}
+
+print(B.difference(A))  # {100, 60}
