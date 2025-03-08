c645afcbc84f0eed185587cf02f317d3cc6d6c04
itsme-swe <me.harshmehra07@gmail.com>
2025-03-08 09:54:13 +0530

    update

---
+++ 11_FunctionsInPython/3_defaultArguments.py
@@ -0,0 +1,16 @@
+# Default Argument
+
+
+def add(a, b=0, c=0):
+
+    return a + b + c
+
+
+sum = add(5, 5)
+
+print(sum)
+
+"""
+🔸 default argument should always be on right side and keyword should be on left side.
+And, default argument can be created only once.
+"""
