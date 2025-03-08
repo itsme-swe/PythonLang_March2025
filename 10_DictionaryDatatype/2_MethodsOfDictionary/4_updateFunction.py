898558e73931e9ff6a471373af51e08155ae2b7e
itsme-swe <me.harshmehra07@gmail.com>
2025-03-08 05:58:29 +0530

    update

---
+++ 10_DictionaryDatatype/2_MethodsOfDictionary/4_updateFunction.py
@@ -0,0 +1,7 @@
+d1 = {1: "James", 2: "Mukul", 3: "Rajat", 4: "Sohan", 5: "Vipul"}
+
+d2 = {6: 69.5, 7: "weight"}
+
+d1.update(d2)
+
+print(d1) # {1: 'James', 2: 'Mukul', 3: 'Rajat', 4: 'Sohan', 5: 'Vipul', 6: 69.5, 7: 'weight'}
