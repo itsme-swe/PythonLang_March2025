c3d5c218d6a1b807940d51952e2e3944ccf4a659
itsme-swe <me.harshmehra07@gmail.com>
2025-03-09 13:37:56 +0530

    update

---
+++ 12_ModuleInPython/ModuleOne.py
@@ -0,0 +1,21 @@
+total_Amount = 150000000000
+
+
+def add(a, b: int) -> int:
+    return a + b
+
+
+def sub(x, y: int) -> int:
+    return x - y
+
+
+'''
+🔸 The below code will only execute when we'll this cureent file as program file but if we import this file in some another program the below code will not execute.
+'''
+if __name__ == "__main__":
+
+    print(__name__)
+
+    print(f"From module one {add(10,20)}")
+
+    print(f"From module one {abs(sub(10,30))}")
---
+++ 12_ModuleInPython/MyProgram.py
@@ -0,0 +1,9 @@
+import ModuleOne as mo1
+
+n1 = 30
+
+n2 = 50
+
+print(f"The sum of n1 + n2 is {mo1.add(n1,n2)}")
+
+print(f"The difference of n1 - n2 is {abs(mo1.sub(n1,n2))}")
