f90a68be01d9f259bcb99a9976bbfa636a801a5f
itsme-swe <me.harshmehra07@gmail.com>
2025-03-10 08:57:10 +0530

    update

---
+++ 13_ExceptionHandling/1_syntaxError.py
@@ -0,0 +1,16 @@
+a = int(input("Enter the value of a:"))
+b = int(input("Enter the value of b:"))
+
+c = a // b
+
+if c > 0
+  print(c)
+
+'''
+$ py 1_syntaxError.py 
+  File "D:\Python_March2025\13_ExceptionHandling\1_syntaxError.py", line 6
+    if c > 0
+            ^
+SyntaxError: expected ':'
+
+'''
---
+++ 13_ExceptionHandling/2_runtimeError.py
@@ -0,0 +1,16 @@
+a = int(input("Enter the value of a:"))
+b = int(input("Enter the value of b:"))
+
+c = a // b
+
+print(c)
+print(d)
+
+'''
+🔸 Runtime error - 
+Traceback (most recent call last):
+  File "D:\Python_March2025\13_ExceptionHandling\2_runtimeError.py", line 7, in <module>
+    print(d)
+          ^
+NameError: name 'd' is not defined. Did you mean: 'id'?
+'''
