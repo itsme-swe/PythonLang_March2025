4507446b93de773a9d0b7df753abac171544b971
itsme-swe <me.harshmehra07@gmail.com>
2025-03-13 19:21:54 +0530

    update

---
+++ 15_OOP's/5_Polymorphisim/1_duckTyping_Ex1.py
@@ -0,0 +1,22 @@
+def Driver(car):
+
+    car.drive()
+
+
+class Creta:
+
+    def drive(self):
+        print("Creta is driving")
+
+
+class Toyota:
+
+    def drive(self):
+        print("Toyota is driving")
+
+
+Toy = Toyota()
+
+Driver(Toy)
+
+# Toyota is driving
---
+++ 15_OOP's/5_Polymorphisim/2_duckTyping_Ex2.py
@@ -0,0 +1,41 @@
+def PetLover(pet):
+
+    pet.talk()
+
+    if hasattr(pet, "walk"):
+        pet.walk()
+
+
+class Duck:
+    def talk(self):
+        print("Duck is talking")
+
+    def walk(self):
+        print("Duck is walking")
+
+
+class Dog:
+    def talk(self):
+        print("Dog is talking")
+
+
+d1 = Duck()
+PetLover(d1)
+
+# Duck is talking
+# Duck is walking
+
+print()
+
+dg1 = Dog()
+PetLover(dg1)
+
+"""
+When we run this dg1 object we'll be getting an error:
+
+🔸 AttributeError: 'Dog' object has no attribute 'walk'. Did you mean: 'talk'?
+
+🔸 To avoid this error we'll inserting an conditon in our PerLover() function.
+"""
+
+# Now after putting condition in our function we received this proper output " Dog is talking  "
