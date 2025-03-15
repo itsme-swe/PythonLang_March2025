b2baeb4e05796ee7b61a2c53184bbed30cb16f43
itsme-swe <me.harshmehra07@gmail.com>
2025-03-15 05:54:11 +0530

    update

--- 15_OOP's/5_Polymorphisim/3_methodOverloading.py
+++ 15_OOP's/5_Polymorphisim/3_methodOverloading.py
@@ -19,6 +19,6 @@ print(a1.sum(10, 20, 20))  # 50
 """
 🔸 Here, we are acheiving polymorphism by method overloading. ' sum() ' function is performing calculation upon two parameters as well as for 3 parameters differently.
 
-In python if we create one function with different parameters the last function over shadowed other functions automatically, but in C++ and java we can create one function with different parametrs.  
+In python if we create one function with different parameters the last function shadowed other functions automatically, but in C++ and java we can create one function with different parametrs.  
 
 """
